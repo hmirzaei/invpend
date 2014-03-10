19f4fcb4bbe8ec0d9f7e71b1b925b3eeb3bb6f1a
Hamid Mirzaei <hmirzai@gmail.com>
2014-03-10 12:41:58 -0700

    code cleaned up

--- invpend.c
+++ invpend.c
@@ -1,5 +1,6 @@
 #define PWM_FREQ 15e3  //Hz
 #define CTRL_SAMP_TIME 1e-3 //Sec
+#define PWM_MAX 0.99
 
 #define TIC   GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0x40);
 #define TOC   GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0x00);
@@ -8,6 +9,12 @@
 #define RIGHT GPIO_PIN_3
 #define UP    GPIO_PIN_0
 #define DOWN  GPIO_PIN_1
+
+#define A1 0.3913
+#define A2 0.7827
+#define A3 0.3913
+#define B1 0.3695
+#define B2 0.1958
 
 
 #include <string.h>
@@ -27,6 +34,11 @@
 #include "driverlib/adc.h"
 
 
+enum Mode {
+  Open = 0,
+  Stab = 1,
+} mode;
+
 volatile int timerFlag;
 
 int ulPeriod;
@@ -34,19 +46,9 @@ char str[40];
 const int encStates[4] = {0,1,3,2};
 
 double pwm;
-double curr, currErr, currErrInt, currSetPnt;
-double spd, spdErr, spdErrInt, spdSetPnt;
-double pos, posErr, posSetPnt;
-double pendSpd;
-double pendPos;
-enum Mode {
-  Open = 0,
-  Stab = 1,
-} mode;
-  
-int counter;
-int flag;
+double pendSpd, pendPos, pos, spd;
 
+  
 int motEncPrevState;
 long motEncAngle;
 long motEncPeriod;
@@ -58,11 +60,6 @@ long pendEncPeriod;
 int pendPrevDiff;
 
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
 #ifdef DEBUG
 void
 __error__(char *pcFilename, unsigned long ulLine)
@@ -81,31 +78,7 @@ inline void writePwm(double pwm) {
   }
 }
 
-inline double readCurrent() {
-  unsigned long ulADC0_Value[8];
-  int pinVal;
 
-  pinVal = GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_4|GPIO_PIN_5);
-
-  ADCProcessorTrigger(ADC0_BASE, 0);
-  while(!ADCIntStatus(ADC0_BASE, 0, false)){
-  }
-  ADCIntClear(ADC0_BASE, 0);
-  ADCSequenceDataGet(ADC0_BASE, 0, ulADC0_Value);
-
-  if (pinVal == 0x10) {
-    return ulADC0_Value[0] * -0.002643849203542  +0.328681546195706;	  
-  } else {
-    return ulADC0_Value[0] * 0.002664294671601   +0.061590507306012;
-  }
-} 
-
-//*****************************************************************************
-//
-// The interrupt handler for the first timer interrupt.
-//
-//*****************************************************************************
-double prevPwmInc = 0;
 void Timer0IntHandler(void)
 {
   TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
@@ -117,9 +90,6 @@ void GPIOBIntHandler(void)
 {
   int state;
   int diff;
-  //
-  // Clear the GPIO interrupt.
-  //
   GPIOPinIntClear(GPIO_PORTB_BASE, GPIO_PIN_2|GPIO_PIN_3);    
 
   state = GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_2|GPIO_PIN_3) >> 2;
@@ -148,9 +118,6 @@ void GPIODIntHandler(void)
 {
   int state;
   int diff;
-  //
-  // Clear the GPIO interrupt.
-  //
   GPIOPinIntClear(GPIO_PORTD_BASE, GPIO_PIN_6|GPIO_PIN_5);    
 
   state = GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_6|GPIO_PIN_5) >> 5;
@@ -190,7 +157,6 @@ void init(void) {
   GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6);
   GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_2|GPIO_PIN_3);
   GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_6|GPIO_PIN_5);
-  //GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, LEFT|RIGHT|UP|DOWN);
   GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_2|GPIO_PIN_3, GPIO_BOTH_EDGES);
   GPIOIntTypeSet(GPIO_PORTD_BASE, GPIO_PIN_6|GPIO_PIN_5, GPIO_BOTH_EDGES);
   GPIOPinIntEnable(GPIO_PORTB_BASE, GPIO_PIN_2|GPIO_PIN_3);
@@ -239,9 +205,6 @@ void init(void) {
 
 
   //Int
-  /* IntPriorityGroupingSet(3);  */
-  /* IntPrioritySet(INT_TIMER0A, 1<<5); */
-  /* IntPrioritySet(INT_GPIOB, 1); */
   IntMasterEnable();
 
 }
@@ -250,21 +213,16 @@ double u, pu, ppu, y, py, ppy;
 
 int main(void)
 {
-  double pwmInc;
-
   timerFlag = 0;
 
   pwm = 0;
   
+  pendPos = 0;
   pendSpd = 0;
-  currErrInt = 0;
-  currSetPnt = 0; 
+  pos = 0;
   spd = 0;
-  spdErrInt = 0;
-  spdSetPnt = 0; 
-  posSetPnt = 0; 
-  counter = 0;
-  flag = 0;
+
+
   motEncPrevState = 0;
   motEncAngle = 0;
   motEncPeriod = 0xFFFFFFFF;
@@ -273,27 +231,23 @@ int main(void)
   pendEncAngle = -0.287*4000*4;
   pendEncPeriod = 0xFFFFFFFF;
   pendPrevDiff = 0;
+
   mode = Open;
+
   init();
 
   usprintf(str, "Started !!!");
   RIT128x96x4StringDraw(str , 10, 24, 15);
 
   while(1){
+
+    // wait for new control sampling time
     while (!timerFlag) {
     }
     timerFlag = 0;
     TIC;
-    /* usprintf(str, "pwm = %6d",  (int)(pwm*1000)); */
-    /* RIT128x96x4StringDraw(str , 10, 24, 15); */
-    /* usprintf(str, "mot = %6d",  (int)(pos*1000)); */
-    /* RIT128x96x4StringDraw(str , 10, 34, 15); */
-    /* usprintf(str, "pend = %6d",   (int)(pendPos*1000)); */
-    /* RIT128x96x4StringDraw(str , 10, 44, 15); */
-    /* usprintf(str, "curr = %6d",   (int)(curr*1000)); */
-    /* RIT128x96x4StringDraw(str , 10, 54, 15); */
 
-
+    // push button actions
     if (GPIOPinRead(GPIO_PORTE_BASE, LEFT) == 0) {
       pwm = 0.4;
       writePwm(pwm);
@@ -311,6 +265,7 @@ int main(void)
       }
     }
     
+    // safety conditions
     if ((mode==Stab) && (pos > 3.2 || pos < -3.2 || pendPos < -0.25 || pendPos > 0.25)) {
       RIT128x96x4StringDraw(" !!!! HALTED !!!!" , 10, 64, 15);
       IntMasterDisable();
@@ -321,6 +276,7 @@ int main(void)
     }
 
     if (mode==Open) {
+      //updating pendulum and motor position and speed vars
       if (pendEncPeriod != 0) {
 	pendSpd = 50000000.0/((double)pendEncPeriod)/4000/4;
       } else {
@@ -335,6 +291,7 @@ int main(void)
       }
       pos = motEncAngle/720.0;
 
+      // display position and speed vars on LCD 
       usprintf(str, "pend = %6d",   (int)(pendPos*1000));
       RIT128x96x4StringDraw(str , 10, 14, 15);
       usprintf(str, "pendspd=%6d",   (int)(pendSpd*1000));
@@ -347,6 +304,7 @@ int main(void)
 
     if (mode==Stab) {
 
+      //updating pendulum and motor position and speed vars
       if (pendEncPeriod != 0) {
 	pendSpd = 50000000.0/((double)pendEncPeriod)/4000/4;
       } else {
@@ -361,102 +319,23 @@ int main(void)
       }
       pos = motEncAngle/720.0;
 
-#define k1 (-50)
-#define k2 (.5)
-#define a1 4.0
-#define a2 12.0
 
-#define K1 (-k1*a2/((1+k1)*a1))
-#define K2 (-k2*a1/((1+k1)*a1))
-#define K3 (-k2*a2/((1+k1)*a1))
-/* #define K1 -2 */
-/* #define K2 0 */
-/* #define K3 0 */
-
-//      posSetPnt = K1 * pendPos;// + K2 * spd + K3 * pendSpd;
-
-#define A1 0.3913
-#define A2 0.7827
-#define A3 0.3913
-#define B1 0.3695
-#define B2 0.1958
-
+      // filtering pendSpd using a low pass filter
       u = pendSpd;
       y = A1 * u + A2 * pu + A3 * ppu - B1 * py - B2 * ppy; 
-
       ppu = pu;
       pu = u;
       ppy = py;
       py = y;
 
+      // control law
       pwm = 100*pendPos-1.5*y;
-
-      
-#define DEAD_ZONE 0.00
-#define START_PWM 0
-#define PWM_MAX 0.99
-
-
-#define ACC_MAX 1
-
-
-      /* posErr = posSetPnt-pos; */
-
-      /* if (posErr > -DEAD_ZONE && posErr < DEAD_ZONE) { */
-      /* 	posErr = 0; */
-      /* } */
-
-      /* if (posErr > 0) { */
-      /* 	pwm = START_PWM; */
-      /* } else { */
-      /* 	pwm = -START_PWM; */
-      /* } */
-
-      /* pwmInc = 6*posErr; */
-      /* if (pwmInc-prevPwmInc > ACC_MAX) { */
-      /* 	pwmInc = prevPwmInc + ACC_MAX; */
-      /* } else if (pwmInc-prevPwmInc < -ACC_MAX) { */
-
-      /* 	pwmInc = prevPwmInc-ACC_MAX; */
-      /* } */
-      /* pwm += pwmInc; */
-      /* prevPwmInc = pwmInc; */
-
       if (pwm>PWM_MAX) {
       	pwm=PWM_MAX;
       } else if (pwm <-PWM_MAX) {
       	pwm=-PWM_MAX;
       }
-
-      /* if (posErr > -DEAD_ZONE && posErr < DEAD_ZONE) { */
-      /* 	pwm = 0; */
-      /* } else { */
-      /* 	if (posErr > 0) { */
-      /* 	  pwm = PWM_MAX; */
-      /* 	} else { */
-      /* 	  pwm = -PWM_MAX; */
-      /* 	} */
-      /* } */
       writePwm(pwm);
-
-      
-
-      /* counter = counter + 1; */
-      /* if (counter == 3000) { */
-      /* 	if (!flag) { */
-      /* 	  flag = 1; */
-      /* 	} else { */
-      /* 	  flag = 0; */
-      /* 	} */
-      /* 	counter = 0; */
-      /* } */
-      /* if (!flag) { */
-      /* 	posSetPnt = .2; */
-      /* } else { */
-      /* 	posSetPnt = -.2; */
-      /* } */
-      /* posSetPnt += 0.1*sin(5*counter/3000.0*2*3.1415); */
-
     }
 
     TOC;
