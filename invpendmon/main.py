07b3a5a67b179d5d88b3aba8a373cd154612bed4
Hamid Mirzaei <hmirzai@gmailc.om>
2014-03-24 16:11:07 -0700

    Add variable scaling and plots

--- invpendmon/main.py
+++ invpendmon/main.py
@@ -44,8 +44,62 @@ import pickle
 # data = data.reshape(len(data)/5,5)
 #
 # pickle.dump(data, open('save.p', 'wb'), 2)
-data = pickle.load(open('save.p', 'rb'))
+data = pickle.load(open("save.p", 'rb'))
 
-plt.plot(data)
+data2 = np.zeros(data.shape, float)
+for i in range(data.shape[0]):
+    for j in range(data.shape[1]):
+        if data[i,j]> (1<<31):
+            data2[i,j] = data[i,j] - (1<<32)
+        else:
+            data2[i,j] = data[i,j]
+
+print(data[:,0])
+data2[:,0] = 50000000.0/data2[:,0]/4000/4
+data2[:,1] = data2[:,1]/4000.0/4
+data2[:,2] = 50000000.0/data2[:,2]/720
+data2[:,3] = data2[:,3]/720.0
+data2[:,4] = -data2[:,4]/3333.33
+
+t = np.array(range(data2.shape[0]))*3e-3
+
+plt.subplot(5,1,1)
+plt.plot(t,data2[:,1]*360)
+plt.title('Pendulum Angle')
+plt.title('Pendulum Angular Speed')
+plt.ylabel('deg/s')
+plt.grid()
+plt.ylabel('deg')
+
+plt.subplot(5,1,2)
+plt.plot(t,data2[:,0]*360)
+plt.title('Pendulum Angular Speed')
+plt.grid()
+plt.ylabel('deg/s')
+
+plt.subplot(5,1,3)
+plt.plot(t,data2[:,3]*360)
+plt.title('Motor Angle')
+plt.grid()
+plt.ylabel('deg')
+
+
+plt.subplot(5,1,4)
+plt.plot(t,data2[:,2]*360)
+plt.title('Motor Angular Speed')
+plt.grid()
+plt.ylabel('deg/s')
+
+plt.subplot(5,1,5)
+plt.plot(t,data2[:,4]*100)
+plt.title('PWM Command')
+plt.grid()
+plt.ylabel('%')
+
+a= 100*data2[:,1]-1.5*data2[:,0]
+a=(a>1).choose(a,1)
+a=(a<-1).choose(a,-1)
+plt.plot(t,a*100)
+
 plt.show()
 
