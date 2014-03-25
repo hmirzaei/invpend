273b9915897df78ad1b30fec87656df22f84d5a2
Hamid Mirzaei <hmirzai@gmail.com>
2014-03-25 14:04:49 -0700

    Uncomment recoding codes

--- invpendmon/main.py
+++ invpendmon/main.py
@@ -8,42 +8,42 @@ import sys
 import math
 import pickle
 
-# REC_LEN = 40000
-#
-# HOST = '128.195.55.252'  # The remote host5
-# PORT = 23  # The same port as used by the server
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print("trying ...")
-# s.connect((HOST, PORT))
-# print("connected")
-# data = []
-# counts = [0]
-# while len(data) < REC_LEN:
-#     if counts[-1] != 501:
-#         time.sleep(0.1)
-#     s.send(bytes("d", 'UTF-8'))
-#     tmpData = s.recv(10000)
-#     data.extend(list(tmpData)[1:])
-#     counts.extend([len(list(tmpData))])
-#     sys.stdout.write("\r%d%%" %(float(len(data))/REC_LEN*100))    # or print >> sys.stdout, "\r%d%%" %i,
-#     sys.stdout.flush()
-#
-# print("max count: ", max(counts))
-# print(counts)
-# plt.plot(counts)
-# plt.show()
-#
-# print("done")
-# print(len(data))
-#
-# data = data[:math.floor(len(data)/20)*20]
-# data2 = [data[i:i+4] for i in range(0,math.floor(len(data)/4),4)]
-# print('\n'.join([ str(myelement) for myelement in data2[::5] ]))
-# data = np.array(data)
-# data = data[0::4] + (data[1::4] << 8) + (data[2::4] << 16) + (data[3::4] << 24)
-# data = data.reshape(len(data)/5,5)
-#
-# pickle.dump(data, open('save.p', 'wb'), 2)
+REC_LEN = 40000
+
+HOST = '128.195.55.252'  # The remote host5
+PORT = 23  # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("trying ...")
+s.connect((HOST, PORT))
+print("connected")
+data = []
+counts = [0]
+while len(data) < REC_LEN:
+    if counts[-1] != 501:
+        time.sleep(0.1)
+    s.send(bytes("d", 'UTF-8'))
+    tmpData = s.recv(10000)
+    data.extend(list(tmpData)[1:])
+    counts.extend([len(list(tmpData))])
+    sys.stdout.write("\r%d%%" %(float(len(data))/REC_LEN*100))    # or print >> sys.stdout, "\r%d%%" %i,
+    sys.stdout.flush()
+
+print("max count: ", max(counts))
+print(counts)
+plt.plot(counts)
+plt.show()
+
+print("done")
+print(len(data))
+
+data = data[:math.floor(len(data)/20)*20]
+data2 = [data[i:i+4] for i in range(0,math.floor(len(data)/4),4)]
+print('\n'.join([ str(myelement) for myelement in data2[::5] ]))
+data = np.array(data)
+data = data[0::4] + (data[1::4] << 8) + (data[2::4] << 16) + (data[3::4] << 24)
+data = data.reshape(len(data)/5,5)
+
+pickle.dump(data, open('save.p', 'wb'), 2)
 data = pickle.load(open("save.p", 'rb'))
 
 data2 = np.zeros(data.shape, float)
@@ -96,10 +96,10 @@ plt.title('PWM Command')
 plt.grid()
 plt.ylabel('%')
 
-a= 100*data2[:,1]-1.5*data2[:,0]
-a=(a>1).choose(a,1)
-a=(a<-1).choose(a,-1)
-plt.plot(t,a*100)
+# a= 100*data2[:,1]-1.5*data2[:,0]
+# a=(a>1).choose(a,1)
+# a=(a<-1).choose(a,-1)
+# plt.plot(t,a*100)
 
 plt.show()
 
